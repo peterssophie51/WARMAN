8c5e6bd79652141eba4046c30c1109013ca6ede4
Sophie Peters <96754496+peterssophie51@users.noreply.github.com>
2026-07-01 13:55:26 +1200

    Stepper with limit switch using AccelStepper

--- StepperWithLimitSwitch/StepperWithLimitSwitch.ino
+++ StepperWithLimitSwitch/StepperWithLimitSwitch.ino
@@ -1,10 +1,14 @@
 // define the output pins the arduino is plugged intoi
+#include "AccelStepper.h"
 #define directionPin 9
 #define stepPin 4
 #define enablePin 7
 #define stepsPerRevolution 200
-const int limitPin = 11;
+const int limitPin = A0;
 int count = 0;
+#define motorInterfaceType 1
+
+AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, directionPin);
 
 void setup() {
   // declare the pins as outputs
@@ -14,7 +18,8 @@ void setup() {
   digitalWrite(enablePin, LOW);
   pinMode(limitPin, INPUT_PULLUP);
   Serial.begin(9600);
-
+  stepper.setMaxSpeed(50);
+  stepper.setAcceleration(10);
 }
 
 void loop() {
@@ -25,12 +30,8 @@ void loop() {
       Serial.println("Limit switch TRIGGERED");
       count++;
       Serial.println(count);
-      for (int i = 0; i < stepsPerRevolution; i++) {
-        	digitalWrite(stepPin, HIGH);
-          delayMicroseconds(4000);
-          digitalWrite(stepPin, LOW);
-          delayMicroseconds(4000);
-  }
+      stepper.move(200);
+      stepper.runToPosition();
   }
   delay(100);
 
