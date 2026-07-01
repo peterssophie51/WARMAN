67bbee58c80e3761c1a6bc2cc6f36ec207f362c6
Sophie Peters <96754496+peterssophie51@users.noreply.github.com>
2026-07-01 15:23:10 +1200

    Disabling steppers when not in use for manual movement

--- MultipleSteppersWithLimitSwitches/MultipleSteppersWithLimitSwitches.ino
+++ MultipleSteppersWithLimitSwitches/MultipleSteppersWithLimitSwitches.ino
@@ -6,10 +6,8 @@
 #define scoopDirectionPin 10
 #define scoopEnablePin 12
 #define scoopStepPin 8
-
 #define armLimitPin A2
 #define scoopLimitPin A5
-
 #define motorInterfaceType 1
 
 AccelStepper armStepper = AccelStepper(motorInterfaceType, armStepPin, armDirectionPin);
@@ -19,23 +17,26 @@ void setup() {
   Serial.begin(9600);
 
   pinMode(armDirectionPin, OUTPUT);
-  pinMode(armEnablePin, OUTPUT);
   pinMode(armStepPin, OUTPUT);
   pinMode(scoopDirectionPin, OUTPUT);
-  pinMode(scoopEnablePin, OUTPUT);
   pinMode(scoopStepPin, OUTPUT);
-
   pinMode(armLimitPin, INPUT_PULLUP);
   pinMode(scoopLimitPin, INPUT_PULLUP);
 
-  digitalWrite(armEnablePin,  LOW);
-  digitalWrite(scoopEnablePin, LOW);
+  armStepper.setEnablePin(armEnablePin);
+  scoopStepper.setEnablePin(scoopEnablePin);
+
+  // Try true first; if motors still buzz/vibrate instead of turning, flip both to false
+  armStepper.setPinsInverted(false, false, true);
+  scoopStepper.setPinsInverted(false, false, true);
 
   armStepper.setMaxSpeed(50);
   armStepper.setAcceleration(10);
   scoopStepper.setMaxSpeed(50);
   scoopStepper.setAcceleration(10);
 
+  armStepper.disableOutputs();
+  scoopStepper.disableOutputs();
 }
 
 void loop() {
@@ -47,18 +48,25 @@ void loop() {
 
   if (armState == LOW) {
     Serial.println("Arm Stepper pressed");
+    armStepper.enableOutputs();
     armStepper.stop();
     armStepper.move(200);
   }
 
   if (scoopState == LOW) {
     Serial.println("Scoop Stepper pressed");
+    scoopStepper.enableOutputs();
     scoopStepper.stop();
     scoopStepper.move(200);
-
   }
 
   armStepper.run();
   scoopStepper.run();
 
+  if (armStepper.distanceToGo() == 0) {
+    armStepper.disableOutputs();
+  }
+  if (scoopStepper.distanceToGo() == 0) {
+    scoopStepper.disableOutputs();
+  }
 }
